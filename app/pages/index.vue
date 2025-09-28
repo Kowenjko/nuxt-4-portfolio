e12463c42690d410dfd909028a4af5d927c4a67a
Kowenjko <ortoswt@gmail.com>
2025-09-28 13:46:08 +0300

    add defineLazyHydrationComponent

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const LazyServices = defineLazyHydrationComponent('visible', () => import('@/components/sections/Services.vue'))
+const LazySkills = defineLazyHydrationComponent('visible', () => import('@/components/sections/Skills.vue'))
+const LazyExperience = defineLazyHydrationComponent('visible', () => import('@/components/sections/Experience.vue'))
+const LazyWorks = defineLazyHydrationComponent('visible', () => import('@/components/sections/Works.vue'))
+const LazyContacts = defineLazyHydrationComponent('visible', () => import('@/components/sections/Contacts.vue'))
+</script>
 
 <template>
   <main>
     <Hero />
-    <Services />
-    <Skills />
-    <Experience />
-    <Works />
-    <Contacts />
+    <LazyServices :hydrate-on-visible="{ rootMargin: '100px' }" />
+    <LazySkills :hydrate-on-visible="{ rootMargin: '100px' }" />
+    <LazyExperience :hydrate-on-visible="{ rootMargin: '100px' }" />
+    <LazyWorks :hydrate-on-visible="{ rootMargin: '100px' }" />
+    <LazyContacts :hydrate-on-visible="{ rootMargin: '100px' }" />
   </main>
 </template>
